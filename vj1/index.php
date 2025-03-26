3ec5817ec2413afb5c459303570a59b3f92e1650
Borna Vukovac <147806017+Bovna@users.noreply.github.com>
2025-03-26 15:24:42 +0100

    Add files via upload

---
+++ vj1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Borna Vukovac">
+    <meta name="description" content="Kriminalistički roman od Dana Browna">
+    <meta name="keywords" content="Da Vinci, knjiga, Dan Brown, triler, kriminalistički roman">
+    <title>Da Vincijev kod</title>
+</head>
+<body>
+
+    <?php
+        echo "<h1>Da Vincijev kod</h1>";
+        echo "<p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>";
+        echo '<a href="https://hr.wikipedia.org/wiki/Da_Vincijev_kod" target="_blank">Više o knjizi</a>';
+    ?>
+    
+</body>
+</html>
+
+<!-- index.php -->
