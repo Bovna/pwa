30d656ed64b29d0e4c145bb31b8ffc5285d02668
Borna Vukovac <147806017+Bovna@users.noreply.github.com>
2025-05-15 15:43:08 +0200

    Add files via upload

---
+++ vj17/bazabv.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2025 at 03:41 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bazabv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `country_code` varchar(2) NOT NULL DEFAULT '',
+  `country_name` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`country_code`, `country_name`) VALUES
+('AD', 'Andorra'),
+('AE', 'United Arab Emirates'),
+('AF', 'Afghanistan'),
+('AG', 'Antigua and Barbuda'),
+('AI', 'Anguilla'),
+('AL', 'Albania'),
+('AM', 'Armenia'),
+('AN', 'Netherlands Antilles'),
+('AO', 'Angola'),
+('AQ', 'Antarctica'),
+('AR', 'Argentina'),
+('AT', 'Austria'),
+('AU', 'Australia'),
+('AW', 'Aruba'),
+('AZ', 'Azerbaijan'),
+('BA', 'Bosnia and Herzegovina'),
+('BB', 'Barbados'),
+('BD', 'Bangladesh'),
+('BE', 'Belgium'),
+('BF', 'Burkina Faso'),
+('BG', 'Bulgaria'),
+('BH', 'Bahrain'),
+('BI', 'Burundi'),
+('BJ', 'Benin'),
+('BM', 'Bermuda'),
+('BN', 'Brunei Darussalam'),
+('BO', 'Bolivia'),
+('BR', 'Brazil'),
+('BS', 'Bahamas'),
+('BT', 'Bhutan'),
+('BV', 'Bouvet Island'),
+('BW', 'Botswana'),
+('BY', 'Belarus'),
+('BZ', 'Belize'),
+('CA', 'Canada'),
+('CC', 'Cocos (Keeling) Islands'),
+('CF', 'Central African Republic'),
+('CG', 'Congo'),
+('CH', 'Switzerland'),
+('CI', 'Ivory Coast'),
+('CK', 'Cook Islands'),
+('CL', 'Chile'),
+('CM', 'Cameroon'),
+('CN', 'China'),
+('CO', 'Colombia'),
+('CR', 'Costa Rica'),
+('CU', 'Cuba'),
+('CV', 'Cape Verde'),
+('CX', 'Christmas Island'),
+('CY', 'Cyprus'),
+('CZ', 'Czech Republic'),
+('DE', 'Germany'),
+('DJ', 'Djibouti'),
+('DK', 'Denmark'),
+('DM', 'Dominica'),
+('DO', 'Dominican Republic'),
+('DS', 'American Samoa'),
+('DZ', 'Algeria'),
+('EC', 'Ecuador'),
+('EE', 'Estonia'),
+('EG', 'Egypt'),
+('EH', 'Western Sahara'),
+('ER', 'Eritrea'),
+('ES', 'Spain'),
+('ET', 'Ethiopia'),
+('FI', 'Finland'),
+('FJ', 'Fiji'),
+('FK', 'Falkland Islands (Malvinas)'),
+('FM', 'Micronesia, Federated States of'),
+('FO', 'Faroe Islands'),
+('FR', 'France'),
+('FX', 'France, Metropolitan'),
+('GA', 'Gabon'),
+('GB', 'United Kingdom'),
+('GD', 'Grenada'),
+('GE', 'Georgia'),
+('GF', 'French Guiana'),
+('GH', 'Ghana'),
+('GI', 'Gibraltar'),
+('GK', 'Guernsey'),
+('GL', 'Greenland'),
+('GM', 'Gambia'),
+('GN', 'Guinea'),
+('GP', 'Guadeloupe'),
+('GQ', 'Equatorial Guinea'),
+('GR', 'Greece'),
+('GS', 'South Georgia South Sandwich Islands'),
+('GT', 'Guatemala'),
+('GU', 'Guam'),
+('GW', 'Guinea-Bissau'),
+('GY', 'Guyana'),
+('HK', 'Hong Kong'),
+('HM', 'Heard and Mc Donald Islands'),
+('HN', 'Honduras'),
+('HR', 'Croatia (Hrvatska)'),
+('HT', 'Haiti'),
+('HU', 'Hungary'),
+('ID', 'Indonesia'),
+('IE', 'Ireland'),
+('IL', 'Israel'),
+('IM', 'Isle of Man'),
+('IN', 'India'),
+('IO', 'British Indian Ocean Territory'),
+('IQ', 'Iraq'),
+('IR', 'Iran (Islamic Republic of)'),
+('IS', 'Iceland'),
+('IT', 'Italy'),
+('JE', 'Jersey'),
+('JM', 'Jamaica'),
+('JO', 'Jordan'),
+('JP', 'Japan'),
+('KE', 'Kenya'),
+('KG', 'Kyrgyzstan'),
+('KH', 'Cambodia'),
+('KI', 'Kiribati'),
+('KM', 'Comoros'),
+('KN', 'Saint Kitts and Nevis'),
+('KP', 'Korea, Democratic People\'s Republic of'),
+('KR', 'Korea, Republic of'),
+('KW', 'Kuwait'),
+('KY', 'Cayman Islands'),
+('KZ', 'Kazakhstan'),
+('LA', 'Lao People\'s Democratic Republic'),
+('LB', 'Lebanon'),
+('LC', 'Saint Lucia'),
+('LI', 'Liechtenstein'),
+('LK', 'Sri Lanka'),
+('LR', 'Liberia'),
+('LS', 'Lesotho'),
+('LT', 'Lithuania'),
+('LU', 'Luxembourg'),
+('LV', 'Latvia'),
+('LY', 'Libyan Arab Jamahiriya'),
+('MA', 'Morocco'),
+('MC', 'Monaco'),
+('MD', 'Moldova, Republic of'),
+('ME', 'Montenegro'),
+('MG', 'Madagascar'),
+('MH', 'Marshall Islands'),
+('MK', 'Macedonia'),
+('ML', 'Mali'),
+('MM', 'Myanmar'),
+('MN', 'Mongolia'),
+('MO', 'Macau'),
+('MP', 'Northern Mariana Islands'),
+('MQ', 'Martinique'),
+('MR', 'Mauritania'),
+('MS', 'Montserrat'),
+('MT', 'Malta'),
+('MU', 'Mauritius'),
+('MV', 'Maldives'),
+('MW', 'Malawi'),
+('MX', 'Mexico'),
+('MY', 'Malaysia'),
+('MZ', 'Mozambique'),
+('NA', 'Namibia'),
+('NC', 'New Caledonia'),
+('NE', 'Niger'),
+('NF', 'Norfolk Island'),
+('NG', 'Nigeria'),
+('NI', 'Nicaragua'),
+('NL', 'Netherlands'),
+('NO', 'Norway'),
+('NP', 'Nepal'),
+('NR', 'Nauru'),
+('NU', 'Niue'),
+('NZ', 'New Zealand'),
+('OM', 'Oman'),
+('PA', 'Panama'),
+('PE', 'Peru'),
+('PF', 'French Polynesia'),
+('PG', 'Papua New Guinea'),
+('PH', 'Philippines'),
+('PK', 'Pakistan'),
+('PL', 'Poland'),
+('PM', 'St. Pierre and Miquelon'),
+('PN', 'Pitcairn'),
+('PR', 'Puerto Rico'),
+('PS', 'Palestine'),
+('PT', 'Portugal'),
+('PW', 'Palau'),
+('PY', 'Paraguay'),
+('QA', 'Qatar'),
+('RE', 'Reunion'),
+('RO', 'Romania'),
+('RS', 'Serbia'),
+('RU', 'Russian Federation'),
+('RW', 'Rwanda'),
+('SA', 'Saudi Arabia'),
+('SB', 'Solomon Islands'),
+('SC', 'Seychelles'),
+('SD', 'Sudan'),
+('SE', 'Sweden'),
+('SG', 'Singapore'),
+('SH', 'St. Helena'),
+('SI', 'Slovenia'),
+('SJ', 'Svalbard and Jan Mayen Islands'),
+('SK', 'Slovakia'),
+('SL', 'Sierra Leone'),
+('SM', 'San Marino'),
+('SN', 'Senegal'),
+('SO', 'Somalia'),
+('SR', 'Suriname'),
+('ST', 'Sao Tome and Principe'),
+('SV', 'El Salvador'),
+('SY', 'Syrian Arab Republic'),
+('SZ', 'Swaziland'),
+('TC', 'Turks and Caicos Islands'),
+('TD', 'Chad'),
+('TF', 'French Southern Territories'),
+('TG', 'Togo'),
+('TH', 'Thailand'),
+('TJ', 'Tajikistan'),
+('TK', 'Tokelau'),
+('TM', 'Turkmenistan'),
+('TN', 'Tunisia'),
+('TO', 'Tonga'),
+('TP', 'East Timor'),
+('TR', 'Turkey'),
+('TT', 'Trinidad and Tobago'),
+('TV', 'Tuvalu'),
+('TW', 'Taiwan'),
+('TY', 'Mayotte'),
+('TZ', 'Tanzania, United Republic of'),
+('UA', 'Ukraine'),
+('UG', 'Uganda'),
+('UM', 'United States minor outlying islands'),
+('US', 'United States'),
+('UY', 'Uruguay'),
+('UZ', 'Uzbekistan'),
+('VA', 'Vatican City State'),
+('VC', 'Saint Vincent and the Grenadines'),
+('VE', 'Venezuela'),
+('VG', 'Virgin Islands (British)'),
+('VI', 'Virgin Islands (U.S.)'),
+('VN', 'Vietnam'),
+('VU', 'Vanuatu'),
+('WF', 'Wallis and Futuna Islands'),
+('WS', 'Samoa'),
+('XK', 'Kosovo'),
+('YE', 'Yemen'),
+('ZA', 'South Africa'),
+('ZM', 'Zambia'),
+('ZR', 'Zaire'),
+('ZW', 'Zimbabwe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_firstname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_country_code` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_firstname`, `user_lastname`, `user_country_code`) VALUES
+(1, 'John', 'Doe', 'TJ'),
+(2, 'Jane', 'Smith', 'PG'),
+(3, 'Bob', 'Johnson', 'PF'),
+(4, 'Alice', 'Williams', 'ER'),
+(5, 'Charlie', 'Brown', 'MM'),
+(6, 'Eva', 'Davis', 'NZ'),
+(7, 'Frank', 'Miller', 'NI'),
+(8, 'Grace', 'Moore', 'MG'),
+(9, 'Henry', 'Taylor', 'GE'),
+(10, 'Isabel', 'Anderson', 'ER'),
+(11, 'Jack', 'Wilson', 'GA'),
+(12, 'Katherine', 'Thomas', 'IR'),
+(13, 'Liam', 'Harris', 'SG'),
+(14, 'Mia', 'Jackson', 'SR'),
+(15, 'Noah', 'White', 'GY'),
+(16, 'Olivia', 'Martin', 'ZR'),
+(17, 'Parker', 'Thompson', 'BN'),
+(18, 'Quinn', 'Allen', 'KE'),
+(19, 'Ryan', 'Young', 'GW'),
+(20, 'Sophia', 'Scott', 'AR'),
+(21, 'Tyler', 'Lewis', 'ST'),
+(22, 'Uma', 'Wright', 'AL'),
+(23, 'Vincent', 'Turner', 'DO'),
+(24, 'Wendy', 'Ward', 'SN'),
+(25, 'Xander', 'Baker', 'FM'),
+(26, 'Yasmine', 'Hill', 'EG'),
+(27, 'Zachary', 'Cooper', 'CR'),
+(28, 'Ava', 'Evans', 'TO'),
+(29, 'Benjamin', 'Fisher', 'KG'),
+(30, 'Chloe', 'Gray', 'WS'),
+(31, 'Daniel', 'Green', 'LC'),
+(32, 'Emma', 'Hall', 'RW'),
+(33, 'Felix', 'Hill', 'SC'),
+(34, 'Gabriella', 'Jones', 'RU'),
+(35, 'Harrison', 'King', 'BR'),
+(36, 'Ivy', 'Lopez', 'BT'),
+(37, 'Jackson', 'Morgan', 'KY'),
+(38, 'Kate', 'Nelson', 'FI'),
+(39, 'Logan', 'Owens', 'DZ'),
+(40, 'Mila', 'Perez', 'LU'),
+(41, 'Nathan', 'Quinn', 'VE'),
+(42, 'Oliver', 'Reed', 'FJ'),
+(43, 'Penelope', 'Roberts', 'CV'),
+(44, 'Quincy', 'Smith', 'BG'),
+(45, 'Riley', 'Turner', 'EG'),
+(46, 'Samantha', 'Underwood', 'SV'),
+(47, 'Thomas', 'Vargas', 'AU'),
+(48, 'Ursula', 'Wells', 'ID'),
+(49, 'Victor', 'Xavier', 'RE'),
+(50, 'Winnie', 'Young', 'GE'),
+(51, 'Xander', 'Zimmerman', 'CN');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`country_code`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
